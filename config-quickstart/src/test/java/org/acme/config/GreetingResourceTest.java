c0ed6a2ba0813bf08d2437fee34a883b9a34c172
Sorin I Costea <sorin-costea@users.noreply.github.com>
2020-05-15 10:32:23 +0200

    logging as recommended ignored in "run as junit", generating warnings run as "mvn install"

--- config-quickstart/src/test/java/org/acme/config/GreetingResourceTest.java
+++ config-quickstart/src/test/java/org/acme/config/GreetingResourceTest.java
@@ -4,14 +4,20 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 public class GreetingResourceTest {
-
+	Logger LOGGER = LoggerFactory.getLogger(GreetingResourceTest.class);
     @Test
     public void testHelloEndpoint() {
+    	LOGGER.trace("TRACE");
+    	LOGGER.debug("DEBUG");
+    	LOGGER.info("INFO");
+    	LOGGER.error("ERROR");
         given()
                 .when().get("/greeting")
                 .then()
